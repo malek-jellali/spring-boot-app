3819fde7ace7cf2cfb8edf6d4d09c02d04a35fc9
malak <malek.jellali@etudiant-fst.utm.tn>
2024-10-26 09:37:20 +0100

    change terraform variables credentials

--- terraform/variables.tf
+++ terraform/variables.tf
@@ -1,31 +1,31 @@
 variable "aws_region" {
-  description = "La région AWS"
+  description = "us-east-1"
   type        = string
   default     = "us-east-1"  # Région mise à jour
 }
 
 variable "cluster_name" {
-  description = "Nom du cluster EKS"
+  description = "terraform-cluster"
   type        = string
-  default     = "mykubernetes"  # Nom du cluster mis à jour
+  default     = "terraform-cluster"  # Nom du cluster mis à jour
 }
 
 variable "subnet_ids" {
   description = "IDs des sous-réseaux"
   type        = list(string)
-  default     = ["subnet-075e0dd45da90bdb8", "subnet-0114974e6dbd8217e"]  # Valeurs par défaut
+  default     = ["subnet-0feceec4f265e1b97", "subnet-0b079d6b9f9a27c15"]  # Valeurs par défaut
 }
 
 variable "role_arn" {
   description = "ARN du rôle IAM pour EKS"
   type        = string
-  default     = "arn:aws:iam::744983671605:role/LabRole"  # Valeur par défaut
+  default     = "arn:aws:iam::756447729578:role/LabRole"  # Valeur par défaut
 }
 
 variable "vpc_id" {
   description = "L'ID du VPC pour le cluster EKS"
   type        = string
-  default     = "vpc-04c984733bc5b455e"  # Remplacez par votre ID de VPC réel
+  default     = "vpc-0d37f28be4d55cf15"  # Remplacez par votre ID de VPC réel
 }
 
 variable "vpc_cidr" {
